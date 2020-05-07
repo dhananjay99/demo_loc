2ca7432d32c0a40fa22e247184c51f3e440577b2
dhananjay99 <dhananjay.gandole1@gmail.com>
2020-05-07 19:53:13 +0530

    testing

---
+++ src/test/java/com/transformhub/demo/DemoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transformhub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
